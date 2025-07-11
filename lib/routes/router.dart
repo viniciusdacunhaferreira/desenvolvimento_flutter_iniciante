9286dc2796576bbd645d0d0fedae79fe8ee7c063
Vinícius da Cunha Ferreira <viniciusdacunha.ferreira@gmail.com>
2025-07-11 20:12:55 -0300

    Aula 23 - Formulários parte 1

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../pages/add_person_page.dart';
 import '../pages/home_page.dart';
-import '../pages/new_page.dart';
 import 'routes.dart';
 
 Map<String, WidgetBuilder> routes = {
   Routes.initialRoute: (BuildContext context) => HomePage(),
-  Routes.newPage: (BuildContext context) => NewPage(),
+  Routes.addPersonPage: (BuildContext context) => AddPersonPage(),
 };
