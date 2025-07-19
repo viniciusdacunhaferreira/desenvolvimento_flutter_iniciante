838a5ed05c0f5269dd8ffa217eaca4782fab2c97
Vinícius da Cunha Ferreira <viniciusdacunha.ferreira@gmail.com>
2025-07-19 11:08:51 -0300

    Formulários parte 2

---
+++ lib/models/person_dto.dart
@@ -0,0 +1,7 @@
+class PersonDto {
+  PersonDto({required this.name, required this.height, required this.weight});
+
+  String name;
+  int height;
+  double weight;
+}
--- lib/pages/add_person_page.dart
+++ lib/pages/add_person_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../extensions/build_context.dart';
+import '../models/person_dto.dart';
 
 class AddPersonPage extends StatefulWidget {
   const AddPersonPage({super.key});
@@ -14,6 +15,17 @@ class _AddPersonPageState extends State<AddPersonPage> {
   final heightController = TextEditingController();
   final weightController = TextEditingController();
 
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    heightController.dispose();
+    weightController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,10 +34,14 @@ class _AddPersonPageState extends State<AddPersonPage> {
         actions: [
           TextButton(
             onPressed: () {
-              print(
-                'Name: ${nameController.text}\nHeight: ${heightController.text}\nWeight: ${weightController.text}',
-              );
-              context.pop();
+              if (formKey.currentState?.validate() ?? false) {
+                final personDto = PersonDto(
+                  name: nameController.text,
+                  height: int.tryParse(heightController.text) ?? 0,
+                  weight: double.tryParse(weightController.text) ?? 0,
+                );
+                context.pop();
+              }
             },
             child: Text('Save'),
           ),
@@ -34,34 +50,56 @@ class _AddPersonPageState extends State<AddPersonPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(24),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: nameController,
-              decoration: InputDecoration(
-                label: Text('Name'),
-                border: OutlineInputBorder(),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  label: Text('Full name'),
+                  border: OutlineInputBorder(),
+                ),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value!.isEmpty == true) return 'Invalid name';
+                  if (value.split(' ').length < 2) return 'Invalid name';
+                  for (String n in value.split(' ')) {
+                    if (n.isEmpty) return 'Invalid name';
+                  }
+                  return null;
+                },
               ),
-            ),
-            SizedBox(height: 16),
-            TextFormField(
-              controller: weightController,
-              decoration: InputDecoration(
-                label: Text('Weight'),
-                border: OutlineInputBorder(),
-                suffix: Text('kg'),
+              SizedBox(height: 16),
+              TextFormField(
+                controller: weightController,
+                decoration: InputDecoration(
+                  label: Text('Weight'),
+                  border: OutlineInputBorder(),
+                  suffix: Text('kg'),
+                ),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value?.isEmpty == true) return 'Invalid weight';
+                  return null;
+                },
               ),
-            ),
-            SizedBox(height: 16),
-            TextFormField(
-              controller: heightController,
-              decoration: InputDecoration(
-                label: Text('Height'),
-                border: OutlineInputBorder(),
-                suffix: Text('cm'),
+              SizedBox(height: 16),
+              TextFormField(
+                controller: heightController,
+                decoration: InputDecoration(
+                  label: Text('Height'),
+                  border: OutlineInputBorder(),
+                  suffix: Text('cm'),
+                ),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value?.isEmpty == true) return 'Invalid height';
+                  return null;
+                },
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
